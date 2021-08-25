995571660f86edbe0a0b03709d7cff04e4564303
Mohammed Imtiyaz <thisisimtiyaz01@gmail.com>
2021-08-25 08:34:47 +0530

    Delete buckets directory

--- buckets/main/bin/test.ps1
+++
@@ -1,4 +0,0 @@
-#requires -Modules @{ ModuleName = 'Pester'; ModuleVersion = '4.4.0' }
-
-if(!$env:SCOOP_HOME) { $env:SCOOP_HOME = resolve-path (split-path (split-path (scoop which scoop))) }
-Invoke-Pester "$psscriptroot/.."
