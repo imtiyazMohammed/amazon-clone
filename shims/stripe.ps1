55c261d518f2a5e4b8ccff7eebc60fd8717bd2e7
Mohammed Imtiyaz <thisisimtiyaz01@gmail.com>
2021-08-25 08:35:13 +0530

    Delete shims directory

--- shims/stripe.ps1
+++
@@ -1,3 +0,0 @@
-﻿if (!(Test-Path Variable:PSScriptRoot)) { $PSScriptRoot = Split-Path $MyInvocation.MyCommand.Path -Parent }
-$path = join-path "$psscriptroot" "..\apps\stripe\current\stripe.exe"
-if($myinvocation.expectingInput) { $input | & $path  @args } else { & $path  @args }
